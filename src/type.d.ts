b79be2df419611f1e2d6722005c60f88848514ba
CL TheDreWen <louis.charneux@gmail.com>
2025-10-31 14:37:27 +0100

    Update CommandDefinition type to support SlashCommandOptionsOnlyBuilder

--- src/type.d.ts
+++ src/type.d.ts
@@ -1,3 +1,3 @@
-import { ButtonInteraction, ChatInputCommandInteraction } from "discord.js";
+import { ButtonInteraction, ChatInputCommandInteraction, SlashCommandOptionsOnlyBuilder } from "discord.js";
 
-export type CommandDefinition = { data: SlashCommandBuilder, execute: (interaction: ChatInputCommandInteraction) => void, buttons?: { id: string, handle: (interaction: ButtonInteraction) => void}[]};
+export type CommandDefinition = { data: SlashCommandBuilder | SlashCommandOptionsOnlyBuilder, execute: (interaction: ChatInputCommandInteraction) => void, buttons?: { id: string, handle: (interaction: ButtonInteraction) => void}[]};
